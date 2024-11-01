af16eacd894e3499369a64c6dd9abd449b075de0
VIJAYDEBBARMA <167344447+VIJAYDEBBARMA@users.noreply.github.com>
2024-11-01 18:42:12 +0530

    Update LoggerStackTraceUtil.java

--- src/main/java/com/devopsdemo/utilities/LoggerStackTraceUtil.java
+++ src/main/java/com/devopsdemo/utilities/LoggerStackTraceUtil.java
@@ -30,7 +30,7 @@ public class LoggerStackTraceUtil {
 		return b.toString(); 
 	}
 
-	// Static Logger function
+	//// Static Logger function
 	public static void printErrorMessage(Throwable th)
 	{
 		try{
